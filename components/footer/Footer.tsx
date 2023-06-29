10655c92b421610ed1362f58025b7165401f94cc
Tien <quangtien05022001@gmail.com>
2023-06-29 18:19:02 +0700

    laptop 29/06/2023

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import { COLOR_WHITE } from "../constants/style-constants"
 import { LIST_KEYWORDS, LIST_MENU } from "./constants"
 import { Col, Row } from "antd"
+import { url } from "inspector"
 export interface FooterProp {}
 
 export default function Footer(props: FooterProp) {
@@ -39,32 +40,74 @@ export default function Footer(props: FooterProp) {
         <div className={styles.footer_left}>
             <ul className={styles.foot_hid}>
               
-              <li className={styles.list}>
-                  <span>Đơn vị chủ quản</span>
+              <li className={styles.non_display}>
+                  <span style={{fontSize:"14px"}}>Đơn vị chủ quản</span>
                   <br />
-                  "Công ty cổ phần thanh toán Hưng Hà"
+                  <p style={{fontSize:"22px"}}>Công ty cổ phần thanh toán Hưng Hà </p>
               </li>
-              <li className={styles.list}>
-                "VP1: "
-                <a >Tầng 4, B50,Lô 6,KĐT Định Công - Hoàng Mai - Hà Nội </a>
+              <div className={styles.flex}>
+              <div>
+              <Image
+                  alt="/"
+                  src={"/Building.png"}
+                  width={30}
+                  height={30}
+                  style={{ marginLeft: "10px" }}
+                />
+              </div>
+              <div>
+              <li className={styles.non_display} style={{padding: "5px 0px 5px 0px"}}>
+                
+                <a >VP1: Tầng 4, B50,Lô 6,KĐT Định Công - Hoàng Mai - Hà Nội </a>
               </li>
-              <li className={styles.list}>
+              <li className={styles.list} style={{paddingLeft:"0px"}}>
                 VP2: Thôn Thanh Miếu,Xã Việt Hưng, Huyện Văn Lâm, Tỉnh Hưng Yên
               </li>
-              <li className={styles.list}>
+
+              <li className={styles.list} style={{paddingLeft:"0px", paddingBottom:"5px" }}>
                 VP3: Tầng 2,Số 1 đường Trần Nguyên Đán,Khu Đô Thị Định Công, Hoàng Mai, Hà Nội
               </li>
-              <li className={styles.list}>
-                <span>Hotline:</span>
-                "0982.079.209"
+              </div>
+              </div>
+              
+              <div className={styles.flex}>
+                <div>
+                  <Image
+                  alt="/"
+                  src={"/Hotline.png"}
+                  width={30}
+                  height={30}
+                  style={{ marginLeft: "10px" }}
+                />
+                </div>
+                <div>
+                  <li className={styles.non_display2} style={{padding: "5px 0 5px 0px" ,}}>
+                  <span>Hotline: 0982.079.209</span>
+                
               </li>
-              <li className={styles.list}>
-                <span>
-                Email hỗ trợ:
-                </span>
-                timviec365.vn@gmail.com
+                </div>
+              </div>
 
-              </li>
+              <div className={styles.flex}>
+                <div>
+                <Image
+                  alt="/"
+                  src={"/Email.png"}
+                  width={30}
+                  height={30}
+                  style={{ marginLeft: "10px" }}
+                />
+                </div>
+                <div>
+                  <li className={styles.non_display3} style={{padding: "7px 0 5px 0px" }}>
+                    <span>
+                    Email hỗ trợ: timviec365.vn@gmail.com
+                    </span>
+                  
+
+                    </li>
+                </div>
+              </div>
               
             </ul>
             <ul className={styles.foot_show}>
@@ -91,6 +134,17 @@ export default function Footer(props: FooterProp) {
               <li className={styles.list}>
                 <a>Quy định giải quyết tranh chấp</a>
               </li>
+              <li className={styles.dcma_icon}>
+                <p className={styles.btc_icon}>
+                  <a className={styles.abc}>
+
+                  </a>
+                  <a className={styles.abc}>
+                    
+                  </a>
+                </p>
+
+              </li>
               
               
               
@@ -99,6 +153,33 @@ export default function Footer(props: FooterProp) {
             </ul>
         </div>
         <div className={styles.footer_right}>
+           <div className={styles.footer_right_left}>
+              <div className={styles.footer_right_top}>
+                  <p className={styles.footer_right_top_p}>Tải app để tìm việc làm siêu tốc</p>
+                  <p className={styles.footer_right_top_p}>Tạo CV đẹp với 365+ mẫu CV xin việc</p>                  
+              </div>
+              <div className={styles.qr_cv}>
+              <Image
+                  alt="/"
+                  src={"/qr.png"}
+                  width={112}
+                  height={112}
+                  style={{ marginLeft: "10px" }}
+                />
+              </div>
+              <div className={styles.qr_cv}>
+              <Image
+                  alt="/"
+                  src={"/qr.png"}
+                  width={112}
+                  height={112}
+                  style={{ marginLeft: "10px" }}
+                />
+              </div>
+              <div className={styles.qr_cv}></div>
+              <div className={styles.footer_right_bot}></div>
+              <div className={styles.footer_right_bot} style={{textAlign:"center"}}></div>
+           </div>
 
         </div>
         <div className={styles.foot_bot}>
